9c695156d386ef4a408bce5866666c1135f76bf3
tejainece <tejainece@gmail.com>
2018-02-23 18:56:03 +0100

    Added when

--- lib/domino_nodes.dart
+++ lib/domino_nodes.dart
@@ -2,3 +2,13 @@ export 'attributes/attributes.dart';
 export 'elements/elements.dart';
 export 'events/events.dart';
 export 'styles/styles.dart';
+export 'package:domino/setters.dart';
+
+dynamic when(condition, result) {
+  if (condition is Function) condition = condition();
+  if (condition) {
+    if (result is Function) return result();
+    return result;
+  }
+  return null;
+}
